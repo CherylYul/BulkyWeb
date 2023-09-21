985a3216203b70d350c1d60c1a71697fccd37592
cherylyul <yulcheryl2357@gmail.com>
2023-09-21 19:20:31 +0700

    add crud performance

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
     public class Category
     { 
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage="Display Order must be from 1 to 100")]
         public int DisplayOrder { get; set; } 
     } 
 }
